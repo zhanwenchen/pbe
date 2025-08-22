57fa06e08827fd157438b5b26eafd1e3d046b1d7
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2025-08-22 16:10:02 -0700

    Update lr_scheduler.py

--- ldm/lr_scheduler.py
+++ ldm/lr_scheduler.py
@@ -45,7 +45,7 @@ class LambdaWarmUpCosineScheduler2:
         self.f_min = f_min
         self.f_max = f_max
         self.cycle_lengths = cycle_lengths
-        self.cum_cycles = np.cumsum([0] + list(self.cycle_lengths))
+        self.cum_cycles = (np.cumsum([0] + list(self.cycle_lengths))).ravel()
         self.last_f = 0.
         self.verbosity_interval = verbosity_interval
 
@@ -63,15 +63,15 @@ class LambdaWarmUpCosineScheduler2:
             if n % self.verbosity_interval == 0: print(f"current step: {n}, recent lr-multiplier: {self.last_f}, "
                                                        f"current cycle {cycle}")
         if n < self.lr_warm_up_steps[cycle]:
-            f = (self.f_max[cycle] - self.f_start[cycle]) / self.lr_warm_up_steps[cycle] * n + self.f_start[cycle]
-            self.last_f = f
+            self.last_f = f = float((self.f_max[cycle] - self.f_start[cycle]) / self.lr_warm_up_steps[cycle] * n + self.f_start[cycle])
+            # self.last_f = f
             return f
         else:
             t = (n - self.lr_warm_up_steps[cycle]) / (self.cycle_lengths[cycle] - self.lr_warm_up_steps[cycle])
             t = min(t, 1.0)
-            f = self.f_min[cycle] + 0.5 * (self.f_max[cycle] - self.f_min[cycle]) * (
-                    1 + np.cos(t * np.pi))
-            self.last_f = f
+            self.last_f = f = float(self.f_min[cycle] + 0.5 * (self.f_max[cycle] - self.f_min[cycle]) * (
+                    1 + np.cos(t * np.pi)))
+            # self.last_f = f
             return f
 
     def __call__(self, n, **kwargs):
@@ -88,11 +88,10 @@ class LambdaLinearScheduler(LambdaWarmUpCosineScheduler2):
                                                        f"current cycle {cycle}")
 
         if n < self.lr_warm_up_steps[cycle]:
-            f = (self.f_max[cycle] - self.f_start[cycle]) / self.lr_warm_up_steps[cycle] * n + self.f_start[cycle]
-            self.last_f = f
+            self.last_f = f = float((self.f_max[cycle] - self.f_start[cycle]) / self.lr_warm_up_steps[cycle] * n + self.f_start[cycle])
+            # self.last_f = f
             return f
         else:
-            f = self.f_min[cycle] + (self.f_max[cycle] - self.f_min[cycle]) * (self.cycle_lengths[cycle] - n) / (self.cycle_lengths[cycle])
-            self.last_f = f
+            self.last_f = f = float(self.f_min[cycle] + (self.f_max[cycle] - self.f_min[cycle]) * (self.cycle_lengths[cycle] - n) / (self.cycle_lengths[cycle]))
+            # self.last_f = f
             return f
-
